9819f1a3b03f37ccbde577fa1021df0f540ea0ee
Kadir Dagli <mehmetkadirdagli27@gmail.com>
2024-10-27 17:35:23 +0300

    Add project files.

---
+++ CSharpuygulama2/Ogrenci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpuygulama2
+{
+    internal class Ogrenci
+    {
+        public void OgrenciPuanHesapla(string ogrenciAd, string ogrenciSoyadi, decimal ders1, decimal ders2 )
+        {
+            Console.WriteLine("Öğrenci bilgileri aşağıdaki gibidir.");
+            Console.WriteLine("ad soyad:  {0} {1}",ogrenciAd, ogrenciSoyadi);
+            decimal ortalama = (ders1+ ders2 ) / 2;
+            if (ortalama < 50)
+            {
+                Console.WriteLine("Ortalama değeriniz : {0} - Kaldınız", ortalama);
+
+            }
+            else 
+            {
+                Console.WriteLine("Ortalama değeriniz : {0} -  Geçtiniz", ortalama);
+            }
+
+        }
+    }
+}
---
+++ CSharpuygulama2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpuygulama2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ogrenciAd = string.Empty;
+            string ogrenciSoyadi = string.Empty;
+            decimal ders1 = 0;
+            decimal ders2 = 0;
+            int ortalama = 0;
+
+            Console.WriteLine("Merhaba  Ortalamasını  hesaplamak istediğiniz öğrencinin  bilgilerini giriniz");
+
+            Console.WriteLine("Ad : ");
+            ogrenciAd = Console.ReadLine();
+
+
+            Console.WriteLine("Soyad : ");
+            ogrenciSoyadi = Console.ReadLine();
+
+            Console.WriteLine("Not 1 : ");
+            ders1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Not 2 : ");
+            ders2 = decimal.Parse(Console.ReadLine());
+
+            Ogrenci ogr = new Ogrenci();
+            ogr.OgrenciPuanHesapla(ogrenciAd,ogrenciSoyadi, ders1, ders2);
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
